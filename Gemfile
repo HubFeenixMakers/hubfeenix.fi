6b8eab894fcf5749d3310ab8dcc857787fe72597
Levon Stepanyan <levonmll@gmail.com>
2022-01-14 13:47:49 +0200

    add alchemy device and section page

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'alchemy_cms'
+gem "alchemy-devise", "~> 5.1"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_111733) do
+ActiveRecord::Schema.define(version: 2022_01_14_113159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -382,6 +382,37 @@ ActiveRecord::Schema.define(version: 2022_01_14_111733) do
     t.boolean "redirect_to_primary_host", default: false, null: false
     t.index ["host", "public"], name: "alchemy_sites_public_hosts_idx"
     t.index ["host"], name: "index_alchemy_sites_on_host"
+  end
+
+  create_table "alchemy_users", id: :serial, force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "login"
+    t.string "email"
+    t.string "language"
+    t.string "encrypted_password", limit: 128, default: "", null: false
+    t.string "password_salt", limit: 128, default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "last_request_at"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.integer "updater_id"
+    t.text "cached_tag_list"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.string "alchemy_roles", default: "member"
+    t.index ["alchemy_roles"], name: "index_alchemy_users_on_alchemy_roles"
+    t.index ["email"], name: "index_alchemy_users_on_email", unique: true
+    t.index ["firstname"], name: "index_alchemy_users_on_firstname"
+    t.index ["lastname"], name: "index_alchemy_users_on_lastname"
+    t.index ["login"], name: "index_alchemy_users_on_login", unique: true
+    t.index ["reset_password_token"], name: "index_alchemy_users_on_reset_password_token", unique: true
   end
 
   create_table "gutentag_taggings", id: :serial, force: :cascade do |t|
