a1ac292597aa5122c7a1e6e608fab4d3275b66b1
Torsten <torsten@rubydesign.fi>
2022-01-25 20:57:05 +0200

    simplifying html generation with some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def background_class(card_section)
+    background = card_section.content_by_name(:background).essence.value
+    return "" unless background
+    case background
+    when "dark_blue"
+      "bg-dark_blue text-white"
+    else
+      "bg-#{background}"
+    end
+  end
+
+  def grid_class(element)
+    grid = element.content_by_name(:grid).essence.value.split("-").first
+    "grid-cols-#{grid}"
+  end
+
 end
