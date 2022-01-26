d041691fb5c54523b538f229cdef9444fa2ce683
Levon Stepanyan <levonmll@gmail.com>
2022-01-26 12:01:09 +0200

    add title_color_class nad subtitle_color_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,33 @@
 module ApplicationHelper
-
+  $background = ""
   def background_class(card_section)
-    background = card_section.content_by_name(:background).essence.value
-    return "" unless background
-    case background
+    $background = card_section.content_by_name(:background).essence.value
+    return "" unless $background
+    case $background
     when "dark_blue"
       "bg-dark_blue text-white"
     else
-      "bg-#{background}"
+      "bg-#{$background}"
     end
   end
-
   def grid_class(element)
     grid = element.content_by_name(:grid).essence.value.split("-").first
     "grid-cols-#{grid}"
   end
-
+  def title_color_class()
+    case $background
+    when "dark_blue"
+      "text-white"
+    else
+      "text-title"
+    end
+  end
+  def subtitle_color_class()
+    case $background
+    when "dark_blue"
+      "text-white"
+    else
+      "text-description"
+    end
+  end
 end
